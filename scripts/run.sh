6856caf74e4836e78526013dcbfab2c7c9edb5c4
mack <hypermail@yandex.ru>
2024-07-19 13:52:42 +0300

    loaddata from dumped_data.json

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,6 @@ whoami
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-python load_cities.py
+python manage.py loaddata dumped_data.json
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
