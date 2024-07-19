a22f74438a40adac825ce228297daaea2220430b
mack <hypermail@yandex.ru>
2024-07-19 13:35:57 +0300

    run.sh hot fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,6 @@ whoami
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-python app/load_cities.py
+python load_cities.py
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module config.wsgi
